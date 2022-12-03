53ab6ad98d8d20b6058633b87d6591d72b1f61c4
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2022-12-03 12:03:47 -0800

    Write getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice(choices){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+let computerSelection = getComputerChoice(choices);
