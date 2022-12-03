a65e65216a495e923bc1f0a674d9e7d096cb05b7
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2022-12-03 12:25:00 -0800

    Write function playRound

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let choices = ["rock", "paper", "scissors"];
-
 function getComputerChoice(choices){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
 function playRound(playerSelection, computerSelection){
-
+    if(computerSelection === playerSelection) {
+        console.log("Tie!")
+    }
+         else if(computerSelection === "Rock" && playerSelection === "Scissors") {
+            console.log("Computer chose " + getComputerChoice(choices)
+             + " which beats " + playerSelection + ". Try again!");
+        }
+             else if(computerSelection === "Paper" && playerSelection === "Rock") {
+                console.log("Computer chose " + getComputerChoice(choices)
+             + " which beats " + playerSelection + ". Try again!");
+            }
+                 else if(computerSelection === "Scissors" && playerSelection === "Paper") {
+                    console.log("Computer chose " + getComputerChoice(choices)
+             + " which beats " + playerSelection + ". Try again!");
+                }
+                    else{console.log("You chose " + playerSelection
+                    + " which beats " + getComputerChoice(choices) + ". Congrats!")}
 }
 
+
+let choices = ["rock", "paper", "scissors"];
 let computerSelection = getComputerChoice(choices);
