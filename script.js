d667ee8aa54ad99c13c2318278ace6e6d5b4521f
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2022-12-03 12:53:18 -0800

    Add var response to playRound to reduce length

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function getComputerChoice(choices){
     return choices[Math.floor(Math.random()*choices.length)];
-}
-
-function playRound(playerSelection, computerSelection){
+ }
+ 
+ function playRound(playerSelection, computerSelection){
+    let response = console.log("Computer chose " + computerSelection
+    + " which beats " + playerSelection + ". Try again!");
     if(computerSelection === playerSelection) {
         console.log("Tie!")
     }
          else if(computerSelection === "Rock" && playerSelection.toUpperCase() === "SCISSORS") {
-            console.log("Computer chose " + computerSelection
-             + " which beats " + playerSelection + ". Try again!");
+          response;
         }
              else if(computerSelection === "Paper" && playerSelection.toUpperCase() === "ROCK") {
-                console.log("Computer chose " + computerSelection
-             + " which beats " + playerSelection + ". Try again!");
+                response;
             }
                  else if(computerSelection === "Scissors" && playerSelection.toUpperCase() === "PAPER") {
-                    console.log("Computer chose " + computerSelection
-             + " which beats " + playerSelection + ". Try again!");
+                   response;
                 }
                     else{console.log("You chose " + playerSelection
                     + " which beats " + computerSelection + ". Congrats!")}
-}
-
-
-let choices = ["rock", "paper", "scissors"];
-let computerSelection = getComputerChoice(choices);
+ }
+ 
+ let choices = ["rock", "paper", "scissors"];
+ let computerSelection = getComputerChoice(choices);
+ 
