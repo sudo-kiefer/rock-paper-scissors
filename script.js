97f0c71f10e8b1b2b1cb0ff9458065e0aae43bb2
sudo-kiefer <73907782+sudo-kiefer@users.noreply.github.com>
2023-01-07 04:42:27 -0800

    Add rps-ui fixes

--- script.js
+++ script.js
@@ -1,58 +1,101 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundCounter = 1;
+let holdScore = false; //display previous score until first round of new game begins
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+const rockTarget = document.querySelector(".rock");
+rockTarget.addEventListener('click', rockFunction);
+function rockFunction() {
+    playRound(playerSelection = "Rock");
+}
+
+const paperTarget = document.querySelector(".paper");
+paperTarget.addEventListener('click', paperFunction);
+function paperFunction() {
+    playRound(playerSelection = "Paper");
+}
+
+const scissorsTarget = document.querySelector(".scissors");
+scissorsTarget.addEventListener('click', scissorsFunction);
+function scissorsFunction() {
+    playRound(playerSelection = "Scissors");
+}
+
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(choices){
     return choices[Math.floor(Math.random()*choices.length)];
  }
 
-const choices = ["Rock", "Paper", "Scissors"];
-const computerSelection = getComputerChoice(choices);
-const playerSelection = prompt("Round " + (i+1) + ": Please choose between Rock, Paper, or Scissors.");
- 
-function playRound(playerSelection, computerSelection){
-    
+
+function playRound(){
+
+    if(holdScore === true) {
+        roundCounter = 1;
+        computerScore = 0;
+        playerScore = 0;
+        holdScore = false;
+        const notification = document.getElementById("notifier");
+        notification.innerText = "Please select an option to begin.";
+    }
+
+    const computerSelection = getComputerChoice(choices);
+
     let response = "Computer chose " + computerSelection
     + " which beats " + playerSelection + ". Try again!";
     if(computerSelection.toUpperCase() === playerSelection.toUpperCase()) {
         console.log("Tie!");
-        i = (i-1);
+        roundCounter = roundCounter;
     }
         else if (playerSelection.toUpperCase() !== "ROCK" && playerSelection.toUpperCase() !== "PAPER" && playerSelection.toUpperCase() !== "SCISSORS") {
             console.log(playerSelection + " is not a valid option. Please try again.");
-            i = (i-1);
         }
             else if(computerSelection === "Rock" && playerSelection.toUpperCase() === "SCISSORS") {
                 console.log(response);
                 computerScore = computerScore + 1;
+                roundCounter = (roundCounter+1);
             }
                 else if(computerSelection === "Paper" && playerSelection.toUpperCase() === "ROCK") {
                     console.log(response);
                     computerScore = computerScore + 1;
+                    roundCounter = (roundCounter+1);
                 }
                     else if(computerSelection === "Scissors" && playerSelection.toUpperCase() === "PAPER") {
                         console.log(response);
                         computerScore = computerScore + 1;
+                        roundCounter = (roundCounter+1);
                     }
-                        else{console.log("You chose " + playerSelection+ " which beats " + computerSelection + ". Congrats!"); playerScore = playerScore + 1;}
- }
+                        else{console.log("You chose " + playerSelection+ " which beats " + computerSelection + ". Congrats!"); 
+                            playerScore = playerScore + 1;
+                            roundCounter = (roundCounter+1);
+                        }
 
-console.log(playRound(playerSelection, computerSelection));
-console.log("Player: " + playerScore);
-console.log("Computer: " + computerScore);
 
-    }
+    if(roundCounter === 5) {
+        if(computerScore > playerScore) {
+            holdScore = true;
+            const notification = document.getElementById("notifier");
+            notification.innerText = "COMPUTER WINS THE GAME! BETTER LUCK NEXT TIME!";
 
-    if(playerScore === computerScore) {
-        alert("Tie Game!")
+ 
+        } else {
+            holdScore = true;
+            const notification = document.getElementById("notifier");
+            notification.innerText = "YOU WIN THE GAME!";
+        }
+    
+            
     }
-    else if(playerScore > computerScore) {
-        alert("You Win!")
-    }
-    else if(playerScore < computerScore) {
-        alert("You Lose. Better luck next time!")
-    }
-}
+    
+
+    const compscore = document.getElementById("computer-score");
+    compscore.innerText = "Computer Score: " + computerScore;
+
+    const playscore = document.getElementById("player-score");
+    playscore.innerText = "Player Score: " + playerScore;
+
+    const roundcount = document.getElementById("round-counter");
+    roundcount.innerText = "Round: " + roundCounter;
+    
+};
 
